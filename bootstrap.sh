ded0b6ea91adc5e2001cafbe20d36416734ad52d
Ziou Fang <zioufang@gmail.com>
2019-04-26 12:30:47 +1000

    add . to dir names

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master;
 
 # source file $1 to file $2, if $1 doesn't already exist in $2
 function echo_source {
-    ! grep -q "$1" .bashrc && echo "source $1" >> $2
+    ! grep -q "$1" $2 && echo "source $1" >> $2
 }
 
 function dotsync {
@@ -12,18 +12,18 @@ function dotsync {
     rsync --exclude "bootstrap.sh" \
         --exclude "README.md" \
         --exclude ".git/" \
-        --exclude "bin/" \
         --exclude "vscode/" \
         -avh --no-perms . ~;
-    
+
     # add bash scripts to .bashrc in a new instance
-    echo_source "~/bash/bash_aliases.sh" "~/.bashrc"
-    echo_source "~/bash/bash_configs.sh" "~/.bashrc"
-    echo_source "~/bash/bash_functions.sh" "~/.bashrc"
+    echo_source "~/.bash/bash_aliases.sh" ~/.bashrc;
+    echo_source "~/.bash/bash_configs.sh" ~/.bashrc;
+    echo_source "~/.bash/bash_functions.sh" ~/.bashrc;
     
-    echo_source "~/.bashrc" "~/.bash_profile"
+    echo_source "~/.bashrc" ~/.bash_profile;
 
     source ~/.bashrc;
+    echo "bash dotfiles activated"
 }
 
 if [ "$1"=="-f" ]; then
