1406e76635fc3124247f1f3c7047ab9171953b43
ziou.fang <ziou.fang>
2019-08-12 14:56:56 +1000

    add i3-gaps

--- .config/i3/i3_setup.sh
+++ .config/i3/i3_setup.sh
@@ -1,5 +1,28 @@
 ## DEBIAN BASED
-sudo apt-get install -y i3 i3status
+
+## i3-gaps
+#!/bin/bash
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev automake
+
+cd /tmp
+
+# clone the repository
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+#sudo apt-get install -y i3 #uncomment to use original i3
+sudo apt-get install -y i3status i3lock i3block
 
 # https://github.com/addy-dclxvi/i3-starterpack
 # compton:		desktop effects like transparency
@@ -12,9 +35,6 @@ sudo apt-get install -y compton rxvt-unicode xsel feh rofi lxappearance rxvt-uni
 
 sudo update-alternatives --set x-terminal-emulator "$(which urxvt)"
 
-#vbox
-xrandr --output Virtual1 --model 1920x1200
 
 
-# TODO polybar i3blocks
 
