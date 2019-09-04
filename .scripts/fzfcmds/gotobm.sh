4f3419a8c2e4a1aa6a224d1d864666aa82addc92
ziou.fang <ziou.fang>
2019-09-04 16:58:34 +1000

    add fzf bookmark cmd

---
+++ .scripts/fzfcmds/gotobm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# launch fuzzy matched bookmark in browser
+
+BOOKMARKS=${HOME}/projects/dotfiles/.scripts/fzfcmds/bookmarks
+FZF_ARGS="--reverse"
+
+# Display the menu and get the selection
+SELECTION=`sed '/^#/d;/^$/d;/^ /d' ${BOOKMARKS} | awk '{print $1}' | ${HOME}/.fzf/bin/fzf ${FZF_ARGS}`
+
+if [ "${SELECTION}" ]; then
+	# Get the text associated with the selection.
+	URL=`grep "${SELECTION}" ${BOOKMARKS} | sed "s/${SELECTION} //"`
+fi
+
+if [ "${URL}" ]; then
+	${BROWSER} ${URL} &
+fi
