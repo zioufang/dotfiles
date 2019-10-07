dbb537df75536a1fc3b431ab77b4b0a1d34719a1
ziou fang <ziou.fang>
2019-10-07 15:04:18 +1100

    more i3lock blur and wallpaper

--- .config/i3lock/blur.sh
+++ .config/i3lock/blur.sh
@@ -2,7 +2,9 @@
 TMPBG=/tmp/screen.png
 LOCK=~/.config/i3lock/lock.png
 RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
- 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=100:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
+PIXELATE=128
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+ffmpeg -i $TMPBG -vf scale=iw/$PIXELATE:ih/$PIXELATE,scale=$PIXELATE*iw:$PIXELATE*ih:flags=neighbor $TMPBG
+exec i3lock -i $TMPBG
 rm $TMPBG
