64f4ac93f0ad407de70c33c1d0d67f764ab97bd3
zioufang <zioufang@gmail.com>
2020-06-10 21:37:00 +1000

    use  -ic

--- .scripts/fzfcmds/gotobm.sh
+++ .scripts/fzfcmds/gotobm.sh
@@ -5,17 +5,15 @@ BOOKMARKS=${HOME}/.scripts/fzfcmds/bookmarks
 FZF_ARGS="--reverse"
 
 # Display the menu and get the selection
-SELECTION=`sed '/^#/d;/^$/d;/^ /d' ${BOOKMARKS} | awk '{print $1}' | ${HOME}/.fzf/bin/fzf ${FZF_ARGS}`
+SELECTION=`sed '/^#/d;/^$/d;/^ /d' ${BOOKMARKS} | awk '{print $1}' | fzf ${FZF_ARGS}`
 
 if [ "${SELECTION}" ]; then
 	# Get the text associated with the selection.
 	URL=`grep "${SELECTION}" ${BOOKMARKS} | sed "s/${SELECTION} //"`
 fi
 
-# lnch to launches a process and moves it out of the process group
-# to ensure when term window closes, the spawned child process is moved out
 if [ "${URL}" ]; then
-	~/go/bin/lnch ${BROWSER} ${URL}
+	lnch ${BROWSER} ${URL}
 fi
 
 exit
